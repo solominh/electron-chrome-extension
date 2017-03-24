45db2e21541cc23c089d97f1340d7abe88504882
solominh <hoangminhftu2@gmail.com>
2017-03-24 18:48:19 +0700

    Show popup iframe when text selected

--- chromeapp/js/content_script.js
+++ chromeapp/js/content_script.js
@@ -1,5 +1,7 @@
 document.addEventListener('mouseup', function (event) {
-  var selectedText = window.getSelection().toString();
+  var selectedText = window.getSelection().toString().trim();
+
+  showCaptureWindow(selectedText, event)
 
   // SHOULD SEND EMPTY STRING IF NO TEXT SELECTED
   // if (selectedText.length <= 0) return;
@@ -12,3 +14,42 @@ document.addEventListener('mouseup', function (event) {
     console.log(response);
   });
 })
+
+
+let captureWindow = null;
+
+function showCaptureWindow(selectedText, event) {
+  if (selectedText.length <= 0) {
+    if (captureWindow) {
+      document.querySelector('body').removeChild(captureWindow)
+      captureWindow = null;
+    }
+    return
+  }
+
+  if (captureWindow) {
+    document.querySelector('body').removeChild(captureWindow)
+    captureWindow = null;
+  }
+
+  // Capture window
+  captureWindow = document.createElement('div')
+  captureWindowStyle = {
+    position: 'fixed',
+    top: `${event.clientY}px`,
+    left: `${event.clientX}px`,
+    maxHeight: '400px',
+    boxShadow: '0px 0px 5px #e1e1e1'
+  }
+  Object.assign(captureWindow.style, captureWindowStyle)
+
+  // Iframe
+  let iframeSite = document.createElement('iframe')
+  iframeSite.src = 'http://localhost:3000/dictionary/' + selectedText
+  iframeSite.width = '400px'
+  iframeSite.height = '600px'
+  iframeSite.frameBorder = 0
+
+  captureWindow.appendChild(iframeSite)
+  document.querySelector('body').appendChild(captureWindow)
+}
